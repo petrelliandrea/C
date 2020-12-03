32cff31b926b2e85b59228c2a95726595fac10dc
Andrea <andrea.petrelli@edu.unito.it>
2020-12-03 19:07:05 +0100

    Format file

--- Old_C/es1.c
+++ Old_C/es1.c
@@ -10,50 +10,50 @@
 
 int main()
 {
-  //test git
-  pid_t pid, ppid, value, childPid;
-  int status, sum = 0, i = 0;
+	//test
+	pid_t pid, ppid, value, childPid;
+	int status, sum = 0, i = 0;
 
-  for (i = 0; i < NUM_KIDS; i++)
-  {
+	for (i = 0; i < NUM_KIDS; i++)
+	{
 
-    switch (value = fork())
-    {
+		switch (value = fork())
+		{
 
-    case -1:
-      fprintf(stderr, "Error #%03d: %s\n", errno, strerror(errno));
-      break;
+		case -1:
+			fprintf(stderr, "Error #%03d: %s\n", errno, strerror(errno));
+			break;
 
-    case 0:
-      pid = getpid();
-      ppid = getppid();
+		case 0:
+			pid = getpid();
+			ppid = getppid();
 
-      srand(getpid());
-      int r = rand() % 6 + 1;
+			srand(getpid());
+			int r = rand() % 6 + 1;
 
-      printf("PID: %d, PPID: %d, i: %d\n", pid, ppid, r);
-      exit(r);
-      break;
-    
-    default:
-      break;
-    }
-  }
+			printf("PID: %d, PPID: %d, i: %d\n", pid, ppid, r);
+			exit(r);
+			break;
 
-  sleep(1);
-  printf("\n");
-  while ((childPid = waitpid(0, &status, 0)) != -1)
-  {
-    if (childPid == 0)
-    {
-      printf("No child terminated\n");
-    }
-    else
-    {
-      sum += WEXITSTATUS(status);
-      printf("PARENT:  PID=%d terminated (the child) with status %d\n", childPid, WEXITSTATUS(status));
-    }
-  }
+		default:
+			break;
+		}
+	}
 
-  printf("Sum: %d\n", sum);
+	sleep(1);
+	printf("\n");
+	while ((childPid = waitpid(0, &status, 0)) != -1)
+	{
+		if (childPid == 0)
+		{
+			printf("No child terminated\n");
+		}
+		else
+		{
+			sum += WEXITSTATUS(status);
+			printf("PARENT:  PID=%d terminated (the child) with status %d\n", childPid, WEXITSTATUS(status));
+		}
+	}
+
+	printf("Sum: %d\n", sum);
 }
